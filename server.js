57ff11f4804c60a339d71f5ffa0abdc3b541352a
pratham <propratham4@gmail.com>
2025-06-12 11:21:44 +0530

    Documented Server Using Swagger

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import cors from "cors";
 
 const server = express();
-const urlIdMapping = new Map();
+server.use(cors());
 server.use(express.json());
 
-//Use req.url or req.originalUrl
+const urlIdMapping = new Map();
+
+try { 
+  var swaggerDocument = YAML.load("./swagger.yaml");
+  console.log("Swagger file loaded successfully");
+} catch (error) {
+  console.error("Failed to load swagger.yaml:", error.message);
+}
+
+
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.post("/", (req, res) => {
   const id = parseInt(Math.random() * Math.pow(10, 5)).toString();
-  const shortenedUrl =
-    req.protocol + "://" + req.get("host") + req.originalUrl + id;
+  const baseUrl = req.protocol + "://" + req.get("host");
+  const shortenedUrl = `${baseUrl}/${id}`;
 
   urlIdMapping.set(id, { redirectionUrl: req.body.url, createdAt: new Date() });
   //res.statusCode = 201
@@ -22,7 +35,7 @@ server.get("/:id", (req, res) => {
     res.redirect(urlIdMapping.get(req.params.id).redirectionUrl);
   } else {
     res.status(404);
-    res.json({ error: "Shortened URL Expired" });
+    res.json({ error: "Shortened URL Not Found or Expired" });
   }
 });
 
@@ -32,8 +45,7 @@ server.listen(3000, () => {
   setInterval(() => {
     const now = new Date();
     for (const [key, value] of urlIdMapping) {
-      console.log(value.createdAt.getTime());
-      console.log(now.getTime());
+      
       if (now.getTime() - value.createdAt.getTime() > 24 * 60 * 60 * 1000) {
         urlIdMapping.delete(key);
       }
